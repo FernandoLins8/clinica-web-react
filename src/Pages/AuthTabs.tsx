c3245a84db01f2b4a55376b0f0c35df00e6de58a
Fernando Lins <fernandolins088@gmail.com>
2023-01-17 08:52:30 -0300

    feat(auth page): update background color and make AuthTab better on smaller screens

--- src/Pages/AuthTabs.tsx
+++ src/Pages/AuthTabs.tsx
@@ -4,8 +4,8 @@ import { SignUp } from '../components/SignUp';
 
 export function AuthTabs() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <Tabs.Root defaultValue="SignIn" className="w-1/4 bg-white shadow-md">
+    <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
+      <Tabs.Root defaultValue="SignIn" className="lg:w-1/4 bg-white shadow-md">
         <Tabs.List className="flex justify-around bg-white shadow-md">
           <Tabs.Trigger className="text-indigo-600 font-medium py-2 px-4 hover:bg-indigo-200" value="SignIn">Sign In</Tabs.Trigger>
           <Tabs.Trigger className="text-indigo-600 font-medium py-2 px-4 hover:bg-indigo-200" value="SignUp">Sign Up</Tabs.Trigger>
